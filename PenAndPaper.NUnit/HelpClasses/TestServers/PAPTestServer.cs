7be82916371ac1154bdab3e081f901d857362741
Pixel Lord <Pixel-Lord@gmx.de>
2019-02-28 20:04:35 +0100

    adding IntegrationTests and a TestServer changing the structure of Startup by adding a Interface an abstrract class and a base class updating 2 NuGets

---
+++ PenAndPaper.NUnit/HelpClasses/TestServers/PAPTestServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.TestHost;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.AspNetCore;
+using PenAndPaper.Interfaces.Startups;
+using PenAndPaper.Abstracts.Setups;
+
+namespace PenAndPaper.NUnit.HelpClasses.TestServers
+{
+  public class PAPTestServer//<TStartup> : IDisposable where TStartup : BaseStartup
+  {
+    private TestServer _server;
+    public HttpClient Client { get; set; }
+
+    public PAPTestServer() => SetUpServerAndClient();
+    
+
+    private void SetUpServerAndClient()
+    {
+      this._server = new TestServer(PAPWebHostBuilder());
+
+      this.Client = _server.CreateClient();
+    }
+
+    private IWebHostBuilder PAPWebHostBuilder()
+    {
+      var contentRootPath = Path.GetFullPath(Path.Combine
+        (Directory.GetCurrentDirectory(), "..", "..", "..", "..", "PenAndPaper"));
+
+      return WebHost.CreateDefaultBuilder<Startup>(args: null)
+        .UseContentRoot(contentRootPath)
+        .UseEnvironment("Development")
+        .UseStartup<Startup>();
+    }
+
+    public void Dispose()
+    {
+      Client.Dispose();
+      _server.Dispose();
+    }
+  }
+}
---
+++ PenAndPaper/Abstracts/Setups/BaseStartup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PenAndPaper.Data;
+using PenAndPaper.Interfaces.Startups;
+
+namespace PenAndPaper.Abstracts.Setups
+{
+  public abstract class BaseStartup : IPAPStartup
+  {
+    public virtual void SetupDataBase(IServiceCollection services, IConfiguration Configuration)
+    {
+      // "DefaultConnection" bezieht sich auf die appsettings connection
+      services.AddDbContext<ApplicationDbContext>(options =>
+          options.UseSqlServer(
+              Configuration.GetConnectionString("DefaultConnection")));
+      services.AddDefaultIdentity<IdentityUser>()
+          .AddEntityFrameworkStores<ApplicationDbContext>();
+
+      // !!!Dependenci Injection for allowing the Framework to take care of the desposing!!!
+      services.AddTransient<ApplicationDbContext>();
+    }
+  }
+}
